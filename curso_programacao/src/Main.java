2d8ea7155c2349ee11e942e8ee108653d195f02d
Rannael Santiago <74615002+rannaelsant@users.noreply.github.com>
2022-08-28 21:03:14 -0300

    Add files via upload

---
+++ curso_programacao/src/Main.java
@@ -0,0 +1,23 @@
+package course;
+
+import java.util.Scanner;
+
+public class Program {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Escolha 3 números: ");
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+		int c = sc.nextInt();
+		
+		int higher = max(a, b, c);
+		
+		showResult(higher);
+		
+		sc.close();
+
+	}
+}
---
+++ curso_programacao/src/application/Program.java
@@ -0,0 +1,44 @@
+package application;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Program {
+	public static void main(String[] args) {
+		
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		double xA, xB, xC, yA, yB, yC;
+		
+		System.out.println("Enter mensaures of Triangle x: ");
+		xA = sc.nextDouble();
+		xB = sc.nextDouble();
+		xC = sc.nextDouble();
+		System.out.println("Enter mensaures of Triangle y: ");
+		yA = sc.nextDouble();
+		yB = sc.nextDouble();
+		yC = sc.nextDouble();
+		
+		double p = (xA + xB + xC) / 2;
+		double areaX = Math.sqrt(p * (p - xA) * (p - xB) * (p - xC));
+		
+		p = (yA + yB + yC) / 2;
+		double areaY = Math.sqrt(p * (p - yA) * (p - yB) * (p - yC));
+		
+		System.out.printf("Triangle X area: %.4f%n", areaX);
+		System.out.printf("Triangle Y area: %.4f%n", areaY);
+		
+		if (areaX > areaY){
+			System.out.println("Large area X");
+		}
+		else {
+			System.out.println("Large area Y");
+		}
+		sc.close();
+		
+	}
+
+}
+
+
+	
